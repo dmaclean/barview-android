bcd6e49f8cbb1a5b06e8b075cbe042f986f3a4c3
Dan MacLean <dmaclean82@gmail.com>
2011-08-24 20:02:32 -0400

    Modified the DetailActivity to use an independent Base64 library, which makes the app compatible with Android 2.1.  Also added functionality to the 'Add to Favorites' button (it also properly shows/hides depending on whether the bar is a favorite and whether the user is logged in.).  Also set up the Activity to fetch images from a new REST service which only sends the image (as a Base64 string) and avoids the XML structure.  This is done because the SAX parser was waaaaay to slow.  This approach is much faster.

--- src/com/barview/DetailActivity.java
+++ src/com/barview/DetailActivity.java
@@ -2,13 +2,6 @@ package com.barview;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.StringReader;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
@@ -22,16 +15,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.barview.base64.Base64;
 import com.barview.constants.BarviewConstants;
-import com.barview.models.BarImage;
 import com.barview.rest.RestClient;
 import com.barview.rest.RestClient.RequestMethod;
 import com.barview.utilities.BarviewUtilities;
-import com.barview.xml.BarImageXMLHandler;
+import com.barview.utilities.FacebookUtility;
 
 public class DetailActivity extends Activity {
-	
-	private BarImage barImage;
 	
 	private ImageView imageView;
 	
@@ -42,16 +33,41 @@ public class DetailActivity extends Activity {
 	private String barId;
 	private String barName;
 	
+	/**
+	 * Flag that dictates whether or not the BILoop thread is running.
+	 */
+	private boolean runLoop = true;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.detail);
 	}
 	
+	/**
+	 * Override the onStop() method to set the loop flag to false, killing it.
+	 */
+	public void onStop() {
+		super.onStop();
+		
+		runLoop = false;
+	}
+	
+	/**
+	 * Override the onDestroy() method to set the loop flag to false, killing it.
+	 */
+	public void onDestroy() {
+		super.onDestroy();
+		
+		runLoop = false;
+	}
+	
 	public void onResume() {
 		super.onResume();
 		
 		Log.i("DetailActivity", "in onResume");
+		
+		runLoop = true;
 		
 		Bundle extras = getIntent().getExtras();
 		barId = extras.getString(BarviewConstants.BAR_ID);
@@ -62,7 +78,7 @@ public class DetailActivity extends Activity {
 		 * this bar is already a favorite.
 		 */
 		favesButton = (Button) findViewById(R.id.button1);
-		if(FavoritesActivity.isFavorite(barId))
+		if(FavoritesActivity.isFavorite(barId) || !FacebookUtility.isLoggedIn())
 			favesButton.setVisibility(View.INVISIBLE);
 		
 		/*
@@ -74,7 +90,7 @@ public class DetailActivity extends Activity {
 
 			public void onClick(View v) {
 				RestClient client = new RestClient(BarviewUtilities.getFavoriteURLForRunMode() + "/" + barId);
-				client.AddHeader("User_id", "dmac");
+				client.AddHeader(BarviewConstants.REST_USER_ID, FacebookUtility.getRESTUserId());
 				
 				try {
 					client.Execute(RequestMethod.POST);
@@ -103,8 +119,39 @@ public class DetailActivity extends Activity {
 		TextView tv = (TextView) findViewById(R.id.detailTitle);
 		tv.setText(barName);
 		
-		BarImageFetcher bif = new BarImageFetcher();
-		bif.execute(barId);
+//		BarImageFetcher bif = new BarImageFetcher();
+//		bif.execute(barId);
+		BILoop loop = new BILoop();
+		loop.execute(barId);
+	}
+	
+	/**
+	 * This is a helper class that spawns a thread to run the BarImageFetcher thread
+	 * every 5 seconds (or whatever BARIMAGE_REFRESH_TIME is configured for).  It
+	 * runs as long as the runLoop variable is true, which is set to true upon entering
+	 * onResume() and set to false on onStop();
+	 * 
+	 * @author dmaclean
+	 *
+	 */
+	class BILoop extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			while(runLoop) {
+				BarImageFetcher bif = new BarImageFetcher();
+				bif.execute(barId);
+				
+				try {
+					Thread.sleep(BarviewConstants.BARIMAGE_REFRESH_TIME);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return null;
+		}
+		
 	}
 	
 	/**
@@ -122,42 +169,34 @@ public class DetailActivity extends Activity {
 		
 		@Override
 		protected String doInBackground(String... params) {
-			RestClient client = new RestClient(BarviewUtilities.getBarImageURLForRunMode() + "/" + params[0]);
+			RestClient client = new RestClient(BarviewUtilities.getBarImageURLForRunMode() + "binary/" + params[0]);
 			
+			Log.i(BarImageFetcher.class.getName(), "Starting GET request for bar image");
+			long start = System.currentTimeMillis();
 			try {
 				client.Execute(RequestMethod.GET);
 				response = client.getResponse();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			try {
-				/* Get a SAXParser from the SAXPArserFactory. */
-	            SAXParserFactory spf = SAXParserFactory.newInstance();
-	            SAXParser sp = spf.newSAXParser();
-	
-	            /* Get the XMLReader of the SAXParser we created. */
-	            XMLReader xr = sp.getXMLReader();
-	            /* Create a new ContentHandler and apply it to the XML-Reader*/
-	            BarImageXMLHandler myExampleHandler = new BarImageXMLHandler();
-	            xr.setContentHandler(myExampleHandler);
-	           
-	            /* Parse the xml-data from our URL. */
-	            xr.parse(new InputSource(new StringReader(response)));
-	            /* Parsing has finished. */
-
-	            /* Our ExampleHandler now provides the parsed data to us. */
-	            barImage = myExampleHandler.getBarImage();
-			}
-			catch(Exception e) {
-				Log.e("doInBackground", e.getMessage());
-			}
+			long end = System.currentTimeMillis();
+			Log.i(BarImageFetcher.class.getName(), "GET request took " + (end-start)/1000.0 + " seconds");
 			
 			return response;
 		}
 		
 		protected void onPostExecute(String result) {
-			InputStream is = new ByteArrayInputStream(barImage.getBarImageBytes());
+			long start = System.currentTimeMillis();
+			InputStream is = null;
+			try {
+				is = new ByteArrayInputStream(Base64.decode(response.getBytes()));
+			}
+			catch(Exception e) {
+				Log.e(BarImageFetcher.class.getName(), e.getMessage());
+				e.printStackTrace();
+			}
+			long end = System.currentTimeMillis();
+			Log.i(BarImageFetcher.class.getName(), "Bar image decoding took " + (end-start)/1000.0 + " seconds.");
 			
 			imageView = (ImageView) findViewById(R.id.detailImage);
 			imageView.setImageDrawable(Drawable.createFromStream(is, ""));
