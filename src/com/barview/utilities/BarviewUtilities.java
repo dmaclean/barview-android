50b8aa22165d86b53be6f6949b704d36a6543239
Dan MacLean <dmaclean82@gmail.com>
2011-11-13 09:36:16 -0500

    Added URLs for dev site.

--- src/com/barview/utilities/BarviewUtilities.java
+++ src/com/barview/utilities/BarviewUtilities.java
@@ -11,6 +11,8 @@ public class BarviewUtilities {
 	public static String getFavoritesURLForRunMode() {
 		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEV_MODE))
 			return BarviewConstants.FAVORITES_URL_DEV;
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.BVDEV_MODE))
+			return BarviewConstants.FAVORITES_URL_BVDEV;
 		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
 			return BarviewConstants.FAVORITES_URL_DEMO;
 		
@@ -25,6 +27,8 @@ public class BarviewUtilities {
 	public static String getFavoriteURLForRunMode() {
 		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEV_MODE))
 			return BarviewConstants.FAVORITE_URL_DEV;
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.BVDEV_MODE))
+			return BarviewConstants.FAVORITE_URL_BVDEV;
 		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
 			return BarviewConstants.FAVORITE_URL_DEMO;
 		
@@ -39,6 +43,8 @@ public class BarviewUtilities {
 	public static String getNearbyBarsURLForRunMode() {
 		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEV_MODE))
 			return BarviewConstants.NEARBYBARS_URL_DEV;
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.BVDEV_MODE))
+			return BarviewConstants.NEARBYBARS_URL_BVDEV;
 		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
 			return BarviewConstants.NEARBYBARS_URL_DEMO;
 		
@@ -53,25 +59,41 @@ public class BarviewUtilities {
 	public static String getBarImageURLForRunMode() {
 		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEV_MODE))
 			return BarviewConstants.BARIMAGES_URL_DEV;
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.BVDEV_MODE))
+			return BarviewConstants.BARIMAGES_URL_BVDEV;
 		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
 			return BarviewConstants.BARIMAGES_URL_DEMO;
 		
 		return BarviewConstants.BARIMAGES_URL;
 	}
 	
+	/**
+	 * Get the appropriate Login URL for the current Run mode.
+	 * 
+	 * @return
+	 */
 	public static String getBarviewLoginURLForRunMode() {
 		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEV_MODE))
 			return BarviewConstants.BARVIEWLOGIN_URL_DEV;
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.BVDEV_MODE))
+			return BarviewConstants.BARVIEWLOGIN_URL_BVDEV;
 		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
 			return BarviewConstants.BARVIEWLOGIN_URL_DEMO;
 		
 		return BarviewConstants.BARVIEWLOGIN_URL;
 	}
 	
+	/**
+	 * Get the appropriate logout URL for the current Run mode.
+	 * 
+	 * @return
+	 */
 	public static String getBarviewLogoutURLForRunMode() {
 		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEV_MODE))
 			return BarviewConstants.BARVIEWLOGOUT_URL_DEV;
-		if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.BVDEV_MODE))
+			return BarviewConstants.BARVIEWLOGOUT_URL_BVDEV;
+		else if(BarviewConstants.RUN_MODE.equals(BarviewConstants.DEMO_MODE))
 			return BarviewConstants.BARVIEWLOGOUT_URL_DEMO;
 		
 		return BarviewConstants.BARVIEWLOGOUT_URL;
